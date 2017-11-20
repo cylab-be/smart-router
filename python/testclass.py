809121ecac9c32b20a48a77ad0a9ca1638d03276
benjamin <benjamin.nicodeme@icloud.com>
2017-11-20 14:50:36 +0100

    Planning updated

--- python/testclass.py
+++ python/testclass.py
@@ -5,12 +5,12 @@ snif = sniffer()
 
 snif.dnsQuerry("10.0.2.15")
 
-db = database()
-db.connect()
-now = datetime.datetime.now()
-values = [str("10.0.2.15"), str("test"), now]
-sql = "INSERT INTO DNSQueries (ip_iot, ip_dst, datetime) VALUES (%s, %s, %s)"
-print (db.execquery(sql, values))
-sql = "select * from  DNSQueries"
-print (db.execquery(sql))
-db.close()
+# db = database()
+# db.connect()
+# now = datetime.datetime.now()
+# values = [str("10.0.2.15"), str("test"), now]
+# sql = "INSERT INTO DNSQueries (ip_iot, ip_dst, datetime) VALUES (%s, %s, %s)"
+# print (db.execquery(sql, values))
+# sql = "select * from  DNSQueries"
+# print (db.execquery(sql))
+# db.close()
