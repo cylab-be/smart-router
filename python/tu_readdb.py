66ce15bd35623b7aa6da0a55fcbc61cfa0612aca
benjamin <benjamin.nicodeme@icloud.com>
2018-01-09 15:24:59 +0100

    TU_readdb - updated - remove test lines

--- python/tu_readdb.py
+++ python/tu_readdb.py
@@ -4,8 +4,6 @@ from tu_basics import tu_basics
 class Executetu_readdb(unittest.TestCase):
     def testTablesExistence(self):
         self.assertTrue(tu_basics.testTablesExistence(self), "Tables do not exist")
-    def testlala(self):
-        self.assertTrue(True)
 
 if __name__ == '__main__':
     unittest.main()
