a5ace078352a9198d8295e018ea1bffdcd4911b2
benjamin <benjamin.nicodeme@icloud.com>
2018-03-28 17:41:21 +0200

    added colors into deploy script for clarity

--- docs/setupSRv2.sh
+++ docs/setupSRv2.sh
@@ -1,54 +1,46 @@
 #!/bin/sh
 
-# Use colors, but only if connected to a terminal, and that terminal
-# supports them.
-if which tput >/dev/null 2>&1; then
-	ncolors=$(tput colors)
-fi
-if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
-	RED="$(tput setaf 1)"
-	GREEN="$(tput setaf 2)"
-	YELLOW="$(tput setaf 3)"
-	BLUE="$(tput setaf 4)"
-	BOLD="$(tput bold)"
-	NORMAL="$(tput sgr0)"
-else
-	RED=""
-	GREEN=""
-	YELLOW=""
-	BLUE=""
-	BOLD=""
-	NORMAL=""
-fi
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NORMAL='\033[0m'
 
+printf "${BLUE}[INFO] - Updating sources ${NORMAL}\n" 
 opkg update
+printf "${GREEN}[INFO] - sources updated ${NORMAL}\n\n"
+
+# Divers
+printf "${BLUE}[INFO] - Installing few tools ${NORMAL}\n" 
+opkg install ca-bundle ca-certificates zsh curl git-http nano
+printf "${GREEN}[INFO] - few tools installed ${NORMAL}\n\n"
 
 #ntp client
-printf "${BLUE}[INFO] - Installing ntp client ${NORMAL}" 
+printf "${BLUE}[INFO] - Installing ntp client ${NORMAL}\n" 
 opkg install ntpd
 /etc/init.d/sysntpd disable
 /etc/init.d/ntpd enable
 /etc/init.d/ntpd start
-printf "${GREEN}[INFO] - ntp client Insatlled and running ${NORMAL}"
+printf "${GREEN}[INFO] - ntp client installed and running ${NORMAL}\n\n"
 
 
 #pkill
-printf "${BLUE}[INFO] - Installing pkill ${NORMAL}" 
+printf "${BLUE}[INFO] - Installing pkill ${NORMAL}\n" 
 opkg install procps-ng-pkill
-printf "${GREEN}[INFO] - pkill Insatlled ${NORMAL}"
+printf "${GREEN}[INFO] - pkill installed ${NORMAL}\n\n"
 
 #htop
-printf "${BLUE}[INFO] - Installing htop ${NORMAL}" 
+printf "${BLUE}[INFO] - Installing htop ${NORMAL}\n" 
 opkg install htop
-printf "${GREEN}[INFO] - htop Insatlled ${NORMAL}"
+printf "${GREEN}[INFO] - htop installed ${NORMAL}\n\n"
 
 #Slack
-printf "${BLUE}[INFO] - Installing slackclient ${NORMAL}" 
+printf "${BLUE}[INFO] - Installing slackclient ${NORMAL}\n" 
 pip3 install slackclient
-printf "${GREEN}[INFO] - slackclient Insatlled ${NORMAL}"
+printf "${GREEN}[INFO] - slackclient installed ${NORMAL}\n\n"
 
 #Scapy
-printf "${BLUE}[INFO] - Installing scapy ${NORMAL}" 
+printf "${BLUE}[INFO] - Installing scapy ${NORMAL}\n" 
 opkg upgrade tar wget
 opkg install python tcpdump unzip
 wget http://www.secdev.org/projects/scapy/files/scapy-latest.tar.gz
@@ -57,22 +49,22 @@ cd scapy-2.1.0
 python setup.py install
 cd ..
 rm -rf scapy*
-printf "${GREEN}[INFO] - scapy Insatlled ${NORMAL}"
+printf "${GREEN}[INFO] - scapy installed ${NORMAL}\n\n"
 
 #sqlite3 + python3
-printf "${BLUE}[INFO] - Installing python3 + sqlite3 ${NORMAL}" 
+printf "${BLUE}[INFO] - Installing python3 + sqlite3 ${NORMAL}\n" 
 opkg install sqlite3-cli python-sqlite3 python3 python3-pip python-pip
-printf "${GREEN}[INFO] - python3 + sqlite3 Insatlled ${NORMAL}"
+printf "${GREEN}[INFO] - python3 + sqlite3 installed ${NORMAL}\n\n"
 
 #pip
-printf "${BLUE}[INFO] - Installing python-dotenv + scapy-python3 ${NORMAL}" 
+printf "${BLUE}[INFO] - Installing python-dotenv + scapy-python3 ${NORMAL}\n" 
 # pip install --upgrade pip
 pip3 install python-dotenv
 pip3 install scapy-python3
-printf "${GREEN}[INFO] - python-dotenv + scapy-python3 Insatlled ${NORMAL}"
+printf "${GREEN}[INFO] - python-dotenv + scapy-python3 installed ${NORMAL}\n\n"
 
 #SmartRouter config
-printf "${BLUE}[INFO] - Installing SmartRouter ${NORMAL}" 
+printf "${BLUE}[INFO] - Installing SmartRouter ${NORMAL}\n" 
 cd /root
 git clone https://github.com/RUCD/smart-router.git 
 ln -s /root/smart-router/ /etc
@@ -80,32 +72,34 @@ cp smart-router/docs/init_script.sh /etc/init.d/smartrouter
 chmod +x /etc/init.d/smartrouter
 /etc/init.d/smartrouter enable
 #TODO cp tempalte.env -> .env and addapt it 
-printf "${GREEN}[INFO] - SmartRouter Insatlled ${NORMAL}"
+printf "${GREEN}[INFO] - SmartRouter installed ${NORMAL}\n\n"
 
 #web
 # php7 php7-cgi php7-fastcgi php7-mod-iconv 
-printf "${BLUE}[INFO] - Installing web server ${NORMAL}" 
+printf "${BLUE}[INFO] - Installing web server ${NORMAL}\n" 
 opkg install lighttpd lighttpd-mod-cgi lighttpd-mod-access lighttpd-mod-fastcgi
 mkdir /var/www
 sed -i 's|"/var"|"/var/www"|' /etc/lighttpd/lighttpd.conf
 sed -i 's|80|81|' /etc/lighttpd/lighttpd.conf
 # sed -i 's|".erb" => "/usr/bin/eruby",|".erb" =>  "/usr/bin/eruby", ".php" =>"/usr/bin/php-cgi",|' /etc/lighttpd/conf.d/30-cgi.conf
-sed -i 's|doc_root = "/www"|doc_root = "/var/www"|' /etc/php.ini
+# sed -i 's|doc_root = "/www"|doc_root = "/var/www"|' /etc/php.ini
 /etc/init.d/lighttpd restart
 /etc/init.d/lighttpd enable
+mkdir /etc/smart-router/logs
 touch /etc/smart-router/logs/alerts.txt
 ln -s /root/smart-router/logs/alerts.txt /var/www
-printf "${GREEN}[INFO] - web server Insatlled ${NORMAL}"
+printf "${GREEN}[INFO] - web server installed ${NORMAL}\n\n"
 
 #Ohmyzsh
 # opkg update
-printf "${BLUE}[INFO] - Installing ohmyzsh ${NORMAL}" 
-opkg install ca-bundle ca-certificates zsh curl git-http nano
+printf "${BLUE}[INFO] - Installing ohmyzsh ${NORMAL}\n" 
+printf "${YELLOW}[INFO] - PLEASE TYPE 'exit' AFTER OHMYZSH HAS FINISHED TO INSTALL TO FINISH THE INSTALL ${NORMAL}\n" 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh) ; exit" 
 sed -i -- 's/ash/zsh/g' /etc/passwd
 sed -i -- 's/robbyrussell/agnoster/g' /root/.zshrc
 sed -i "s/prompt_segment blue black '%~'/prompt_segment blue black '%c'/" ~/.oh-my-zsh/themes/agnoster.zsh-theme
 source ~/.zshrc
+printf "${GREEN}[INFO] - ohmyzsh installed ${NORMAL}\n\n"
 
 
 
--- docs/setupUSB.sh
+++ docs/setupUSB.sh
@@ -1,17 +1,33 @@
 #!/bin/sh
 #setup usb fs
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NORMAL='\033[0m'
+
+printf "${BLUE}[INFO] - Updating sources ${NORMAL}\n" 
 opkg update
+printf "${GREEN}[INFO] - sources updated ${NORMAL}\n\n"
+
+printf "${BLUE}[INFO] - Installing few tools ${NORMAL}\n" 
 opkg install kmod-usb2 kmod-usb-storage kmod-fs-ext4 block-mount
+printf "${GREEN}[INFO] - few tools installed ${NORMAL}\n\n"
+
+
 if [ $(ls /mnt/ | grep -c "sda1") -eq 0 ] ; then :
+	printf "${BLUE}[INFO] - mouting usb stick ${NORMAL}\n" 
 	mkdir /mnt/sda1
 	mount /dev/sda1 /mnt/sda1
 	mkdir -p /tmp/cproot
 	mount --bind / /tmp/cproot
 	tar -C /tmp/cproot -cvf - . | tar -C /mnt/sda1 -xf -
 	umount /tmp/cproot
+	printf "${GREEN}[INFO] - end of mouting operations ${NORMAL}\n\n"
 fi
 
-
+printf "${BLUE}[INFO] - editting /etc/config/fstab to run usb fhs from startup ${NORMAL}\n" 
 echo "config 'global'" > /etc/config/fstab
 echo "	option	anon_swap	'0'" >> /etc/config/fstab
 echo "	option	anon_mount	'0'" >> /etc/config/fstab
@@ -28,6 +44,7 @@ echo "        option options       rw,sync" >> /etc/config/fstab
 echo "        option enabled       1" >> /etc/config/fstab
 echo "        option enabled_fsck  0" >> /etc/config/fstab
 
+printf "${GREEN}[INFO] - USB settings OK ! ${NORMAL}\n\n"
 
 
 
