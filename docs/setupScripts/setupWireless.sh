b1a3c2605537c5513817342b8627e8a3e1f00b96
benjamin <benjamin.nicodeme@icloud.com>
2018-03-30 17:22:03 +0200

    wireless script fixed

--- docs/setupScripts/setupWireless.sh
+++ docs/setupScripts/setupWireless.sh
@@ -9,10 +9,17 @@ printf "${BLUE}[INFO] - Configuring wirelless settings ${NORMAL}\n"
 sed -i "/option disabled '1'/d" /etc/config/wireless
 HOSTNAME=$(cat /etc/config/system |grep -i hostname | awk -F "'" '{print $2}')
 sed -i "s/LEDE/$HOSTNAME/" /etc/config/wireless
+
+HAS_ALREADY_A_PASSWORD=$(cat /etc/config/wireless |grep -i -c "option encryption 'none'")
+
 sed -i "s/encryption 'none'/encryption 'psk-mixed'/" /etc/config/wireless
 PASSWORD=$(strings /dev/urandom | grep -o '[[:alnum:]]' | head -n 30 | tr -d '\n'; echo)
-sed -i "/option encryption 'psk-mixed'/a option key '$PASSWORD'" /etc/config/wireless
-sed -i "s/option key/\\toption key/" /etc/config/wireless
+
+if [ $HAS_ALREADY_A_PASSWORD -gt 0 ] ; then :
+	sed -i "/option encryption 'psk-mixed'/a option key '$PASSWORD'" /etc/config/wireless
+	sed -i "s/option key/\\toption key/" /etc/config/wireless
+fi
+
 sed -i "s/HT20/HT40/" /etc/config/wireless
 
 printf "${GREEN}[INFO] - Configuring wireless settings ${NORMAL}\n" 
