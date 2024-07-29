cc511b361fc4ba03d670176dd447a7769d8de13b
Florian Villeret <florian.villeret@ulb.be>
2024-07-29 14:18:18 +0200

    Start url script at startup

--- src/dashboard/apps.py
+++ src/dashboard/apps.py
@@ -1,6 +1,18 @@
 from django.apps import AppConfig
 
+def startup():
+    import threading
+    from signature_analysis.url import start_capture
+    interface = "en0"
+    thread = threading.Thread(target=start_capture, args=(interface,))
+    thread.daemon = True
+    thread.start()
 
 class DashboardConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'dashboard'
+
+    def ready(self):
+        import os
+        if os.environ.get('RUN_MAIN'):
+            startup()
